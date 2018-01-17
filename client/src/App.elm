9108a717e86bd1c7041c823915f071dbe90f8976
Michael Gold <mgold@spscommerce.com>
2018-01-16 20:49:20 -0500

    Initial view code for Todo App

--- client/src/App.elm
+++ client/src/App.elm
@@ -1,8 +1,108 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text)
+import Html
+    exposing
+        ( Html
+        , div
+        , text
+        , section
+        , header
+        , input
+        , h1
+        , label
+        , ul
+        , li
+        , button
+        , footer
+        , span
+        , strong
+        , a
+        , p
+        )
+import Html.Attributes
+    exposing
+        ( class
+        , placeholder
+        , autofocus
+        , id
+        , type_
+        , for
+        , checked
+        , value
+        , href
+        )
 
 
 main : Html msg
 main =
-    div [] [ text "Hello World!" ]
+    view
+
+
+view : Html msg
+view =
+    div []
+        [ section [ class "todoapp" ]
+            [ header [ class "header" ]
+                [ h1 []
+                    [ text "todos" ]
+                , input
+                    [ class "new-todo", placeholder "What needs to be done?", autofocus True ]
+                    []
+                ]
+            , section [ class "main" ]
+                [ input [ id "toggle-all", class "toggle-all", type_ "checkbox" ] []
+                , label [ for "toggle-all" ] [ text "Mark all as complete" ]
+                , ul [ class "todo-list" ]
+                    [ li [ class "completed" ]
+                        [ div [ class "view" ]
+                            [ input [ class "toggle", type_ "checkbox", checked True ]
+                                []
+                            , label
+                                []
+                                [ text "Taste Elm" ]
+                            , button
+                                [ class "destroy" ]
+                                []
+                            ]
+                        , input [ class "edit", value "Create a TodoMVC template" ] []
+                        ]
+                    , li []
+                        [ div [ class "view" ]
+                            [ input [ class "toggle", type_ "checkbox" ]
+                                []
+                            , label
+                                []
+                                [ text "Buy a unicorn" ]
+                            , button
+                                [ class "destroy" ]
+                                []
+                            ]
+                        , input [ class "edit", value "Rule the web" ] []
+                        ]
+                    ]
+                ]
+            , footer [ class "footer" ]
+                [ span [ class "todo-count" ]
+                    [ strong [] [ text "0" ]
+                    , text " items left"
+                    ]
+                , ul [ class "filters" ]
+                    [ li []
+                        [ a [ class "selected" ] [ text "All" ]
+                        ]
+                    , li []
+                        [ a [] [ text "Active" ]
+                        ]
+                    , li []
+                        [ a [] [ text "Completed" ]
+                        ]
+                    ]
+                , button [ class "clear-completed" ] [ text "Clear completed" ]
+                ]
+            ]
+        , footer [ class "info" ]
+            [ p [] [ text "Double-click to edit a todo" ]
+            , p [] [ text "Template by ", a [ href "http://sindresorhus.com" ] [ text "Sindre Sorhus" ] ]
+            , p [] [ text "Created by ", a [ href "http://todomvc.com" ] [ text "Michael Gold" ] ]
+            ]
+        ]
