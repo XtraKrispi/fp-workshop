b98f1cd083be1f6bfc477177eb03ef4edf5247ab
Michael Gold <mgold@spscommerce.com>
2018-01-25 11:30:59 -0500

    Adding skeleto TEA to Shapes app

--- client/src/Shapes.elm
+++ client/src/Shapes.elm
@@ -1,8 +1,44 @@
 module Shapes exposing (main)
 
-import Html exposing (Html, div)
+import Html exposing (Html, beginnerProgram, div, input, label, text, span)
+import Html.Attributes exposing (type_, name, checked, value)
 
 
-main : Html msg
+type Msg
+    = NoOp
+
+
+type alias Model =
+    {}
+
+
+main : Program Never Model Msg
 main =
-    div [] []
+    beginnerProgram { model = model, update = update, view = view }
+
+
+model : Model
+model =
+    {}
+
+
+update : Msg -> Model -> Model
+update _ model =
+    model
+
+
+view : Model -> Html Msg
+view _ =
+    div []
+        [ div []
+            [ label [] [ input [ type_ "radio", name "shapes", checked True ] [], text "Square" ]
+            , label [] [ input [ type_ "radio", name "shapes" ] [], text "Rectangle" ]
+            ]
+        , div []
+            [ label [] [ text "Side Length: " ]
+            , input [ type_ "number", value "4" ] []
+            ]
+        , div []
+            [ span [] [ text "Area: 16" ]
+            ]
+        ]
